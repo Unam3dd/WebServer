6be1f0d6b70b66d5c9e3a259b65a6877d0c2e5be
sam0verfl0w <stales@student.42angouleme.fr>
2023-03-04 02:35:31 +0100

    Log Http

---
+++ inc/http_colors.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   http_colors.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sam0verfl0w <stales@student.42angouleme.f  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 02:27:43 by sam0verfl0w       #+#    #+#             */
+/*   Updated: 2023/03/04 02:33:07 by sam0verfl0w      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef HTTP_COLORS_HPP
+#define HTTP_COLORS_HPP
+
+#define GREY	"\033[30m"
+#define RED		"\033[31m"
+#define GREEN	"\033[32m"
+#define ORANGE	"\033[33m"
+#define BLUE	"\033[34m"
+#define PURPLE	"\033[35m"
+#define CYAN	"\033[36m"
+#define WHITE	"\033[37m"
+
+#endif
