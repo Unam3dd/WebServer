f796c4321ad8a59502cfce36c0a2fd1840027e7e
sam0verfl0w <stales@student.42angouleme.fr>
2023-03-04 11:19:52 +0100

    update colors

--- inc/http_colors.hpp
+++ inc/http_colors.hpp
@@ -6,7 +6,7 @@
 /*   By: sam0verfl0w <stales@student.42angouleme.f  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 02:27:43 by sam0verfl0w       #+#    #+#             */
-/*   Updated: 2023/03/04 02:33:07 by sam0verfl0w      ###   ########.fr       */
+/*   Updated: 2023/03/04 11:17:43 by sam0verfl0w      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,15 @@
 #define PURPLE	"\033[35m"
 #define CYAN	"\033[36m"
 #define WHITE	"\033[37m"
+#define RESET	"\033[00m"
+#define BGBLACK "\033[40m"
+#define	BGREG	"\033[41m"
+#define BGGREEN "\033[42m"
+#define BGBROW	"\033[43m"
+#define BGBLUE	"\033[44m"
+#define BGPURPLE "\033[45m"
+#define BGCYAN	 "\033[46m"
+#define BGGREY	"\033[47m"
+#define TICK	"\n[\033[0;32m\xE2\x9C\x94\033[0m]"
 
 #endif
