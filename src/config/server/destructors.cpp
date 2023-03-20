587ed7c1b3f20e8cc8dd0a1c4e7177784baf89bc
gab <ldournoi@student.42angouleme.fr>
2023-03-20 16:26:31 +0100

    leaks

--- src/config/server/destructors.cpp
+++ src/config/server/destructors.cpp
@@ -6,7 +6,7 @@
 /*   By: stales <stales@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:51:56 by stales            #+#    #+#             */
-/*   Updated: 2023/03/13 18:52:10 by stales           ###   ########.fr       */
+/*   Updated: 2023/03/20 16:20:01 by ldournoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,8 @@
 
 HttpServerConfig::~HttpServerConfig(void)
 {
+	for (std::vector<HttpRequestConfig*>::iterator it = _reqconfig.begin(); it != _reqconfig.end(); it++)
+	{
+		delete *it;
+	}
 }
--- src/webserver/destructors.cpp
+++ src/webserver/destructors.cpp
@@ -6,15 +6,18 @@
 /*   By: stales <stales@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 11:33:12 by stales            #+#    #+#             */
-/*   Updated: 2023/03/20 06:08:13 by ldournoi         ###   ########.fr       */
+/*   Updated: 2023/03/20 16:19:31 by ldournoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "http_config.hpp"
+#include "http_request_config.hpp"
 #include "webserver.hpp"
 
 WebServer::~WebServer(void)
 {
 	for (std::vector<HttpServerConfig*>::iterator it = _configs.begin(); it != _configs.end(); it++)
+	{
 		delete *it;
+	}
 }
