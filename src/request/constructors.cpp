6e9f7458cc786c8efd8563fd243bd359698ea989
Louis-Gabriel Dournois <ldournoi@student.42angouleme.fr>
2023-03-26 18:32:23 +0200

    fix

--- src/request/constructors.cpp
+++ src/request/constructors.cpp
@@ -8,15 +8,27 @@ HttpRequest::HttpRequest(const std::string& req)
 	std::string line;
 	t_status	status = STATUS_OK;
 
+	if (req.length() == 0 || req.find("\r\n\r\n") == std::string::npos)
+	{
+		std::cerr << FAIL << "[HttpRequest::HttpRequest] Received Invalid Request (CRLF mandatory)" << RESET << std::endl;
+		return ;
+	}
+
 	line = req.substr(0, req.find("\r\n"));
 	status = _parseRequestLine(line);
 	if (status != STATUS_OK)
+	{
 		std::cerr << FAIL << "[HttpRequest::HttpRequest] Error parsing request line" << std::endl;
+		return ;
+	}
 	
 	line = req.substr(req.find("\r\n") + 2, req.length() - req.find("\r\n"));
 	status = _parseHeaders(line);
 	if (status != STATUS_OK)
+	{
 		std::cerr << FAIL << "[HttpRequest::HttpRequest] Error parsing headers" << std::endl;
+		return ;
+	}
 	
 	setBody(req.substr(req.find("\r\n\r\n") + 4, req.length() - req.find("\r\n\r\n")));
 }
